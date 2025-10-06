ddb8dc9f2db6b6ba917586170dc21a39faf86489
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-10-06 03:55:43 +0300

    Update Google Drive URL For My CV

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -15,7 +15,7 @@ function PortfolioComponent() {
   const handleDownload = () => {
     if (typeof window !== "undefined") {
       window.open(
-        "https://drive.google.com/file/d/1a7GVtJpe0eAtZ3VcntH2WvZNf4gDf6fy/view?usp=sharing",
+        "https://drive.google.com/file/d/1ddAvMpk4OC7JjrMtc06qn6DfHBZBSBB0/view?usp=sharing",
         "_blank"
       );
     }
