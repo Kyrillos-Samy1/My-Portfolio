c19055b126214ad4a7aeaace23f64ac772a5d512
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-11-14 21:39:51 +0200

    Update portfolio description
    Includes Node.js in the portfolio's technology stack description.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Kyrillos Portfolio",
   description:
-    "Kyrillos Samy Portfolio – Showcasing my React.js & Next.js projects, skills, and journey!",
+    "Kyrillos Samy Portfolio – Showcasing my Node.js, React.js & Next.js projects, skills, and journey!",
 };
 
 export default function RootLayout({
